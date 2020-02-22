5a6f0a630d4b99ea700969496a3095b1a1a4414d
William matera <wmatera@Williams-MBP.home>
2020-02-22 15:34:42 -0500

    cleanup

---
+++ routes/profile.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  res.sendFile(path.join('/Users/wmatera/Desktop/express-start/public/profile.html'))
+
+});
+
+module.exports = router;
