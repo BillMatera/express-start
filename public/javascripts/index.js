e2e7235dccf0edf16b589621e0874cb3058b80c4
William matera <wmatera@Williams-MBP.home>
2020-02-23 18:06:19 -0500

    changing style

---
+++ public/javascripts/index.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  var div = document.getElementsByClassName('fa-arrow-down');
+  var down = true;
+  
+  $( '.navbar-toggler' ).on('click',function() {
+    if(down){
+      $( '.fa-arrow-down' ).addClass('arrow-up');
+    } else{
+      $( '.fa-arrow-down' ).removeClass('arrow-up');
+    }
+    down = !down;
+  });
+
+});
