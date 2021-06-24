a09e6bf9aa8e9af0ba7f6616cd6e3dd126bc611a
William matera <matera.3@osu.edu>
2021-06-24 13:34:15 -0400

    redo UI

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+function goToGithub() {
+  window.location = "https://github.com/BillMatera";
+}
+
+function goToLinkdIn() {
+  window.location = "https://www.linkedin.com/in/william-matera-osu/";
+}
+
+function freeCodeCamp() {
+  window.location = "https://www.freecodecamp.org/";
+}
